527e973396c9264e2bb8d6401071990e6a7dea6a
alikhanmohammadsajid <alikhanmohammadsajid@gmail.com>
2021-06-04 20:18:00 +0600

    i have create detail card element

--- components/MealCard.js
+++ components/MealCard.js
@@ -5,16 +5,20 @@ export default function MealCard({ navigation }) {
   return (
     <View>
       <Text> this is product card </Text>
+
       <Button
+
         title="See Details"
         onPress={() => {
           /* 1. Navigate to the Details route with params */
           navigation.navigate("Details", {
             itemId: 86,
             otherParam: "anything you want here",
+
           });
         }}
       />
+
     </View>
   );
 }
--- screens/SingleMealScreen.js
+++ screens/SingleMealScreen.js
@@ -1,15 +1,44 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function SingleMealScreen({ navigation, route }) {
   const { itemId, otherParam } = route.params;
 
   console.log(itemId, otherParam);
   return (
-    <View>
-      <Text>this is single product . .....</Text>
+    <View style={styles.container}>
+      <View style={{ borderWidth: 1, borderRadius: 10, padding: 20, borderColor: 'gray' }}>
+        <Image source={{ uri: 'https://i.pinimg.com/600x315/30/42/51/3042517c616f5d3fdb1fa69e41798648.jpg' }} style={{ width: 100, height: 50, borderColor: 'gray' }} />
+
+        <Text style={{ color: 'black', fontSize: 18, margin: 5 }}>
+          ✔ Elements
+      </Text>
+        <Text style={{ color: 'black', fontSize: 18, margin: 5 }}>
+          ✔ Elements
+      </Text>
+        <Text style={{ color: 'black', fontSize: 18, margin: 5 }}>
+          ✔ Elements
+      </Text>
+        <Text style={{ color: 'black', fontSize: 18, margin: 5 }}>
+          ✔ Elements
+      </Text>
+        <Text style={{ color: 'black', fontSize: 18, margin: 5 }}>
+          ✔ Elements
+      </Text>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 3,
+    backgroundColor: 'rgb(173, 216, 230)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10
 
-const styles = StyleSheet.create({});
+
+
+  },
+});
